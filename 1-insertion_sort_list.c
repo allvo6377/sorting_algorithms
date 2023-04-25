d97eeb24c8a1ef24f9e978344f26747efa76f18e
allvo6377 <alvinpilot@gmail.com>
2023-04-25 13:40:04 -0700

    quick correction

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,39 +1,49 @@
 #include "sort.h"
 
 /**
- * insertion_sort_list - Sorts a doubly linked list of integers in ascending
- *                       order using the Insertion sort algorithm
- *
- * @list: Pointer to a pointer to the head of the list
- *
- * Return: void
+ * swap_nodes - swaps two nodes in a doubly linked list
+ * @list: pointer to the head of the list
+ * @node: pointer to the node to swap with the next node
+ */
+void swap_nodes(listint_t **list, listint_t *node)
+{
+	listint_t *tmp;
+
+	tmp = node->next;
+	if (node->prev)
+		node->prev->next = tmp;
+	else
+		*list = tmp;
+	tmp->prev = node->prev;
+	node->next = tmp->next;
+	if (tmp->next)
+		tmp->next->prev = node;
+	tmp->next = node;
+	node->prev = tmp;
+}
+
+/**
+ * insertion_sort_list - sorts a doubly linked list of integers
+ * in ascending order using the Insertion sort algorithm
+ * @list: pointer to the head of the list
  */
 void insertion_sort_list(listint_t **list)
 {
-        listint_t *curr = *list, *next = NULL;
+	listint_t *curr, *prev;
 
-        while (curr != NULL)
-        {
-                next = curr->next;
-
-                while (curr->prev != NULL && curr->prev->n > curr->n)
-                {
-                        curr->prev->next = curr->next;
-
-                        if (curr->next != NULL)
-                                curr->next->prev = curr->prev;
-
-                        curr->next = curr->prev;
-                        curr->prev = curr->prev->prev;
-                        curr->next->prev = curr;
-
-                        if (curr->prev == NULL)
-                                *list = curr;
-
-                        print_list(*list);
-                }
-
-                curr = next;
-        }
+	if (!list || !(*list) || !(*list)->next)
+		return;
+	curr = (*list)->next;
+	while (curr)
+	{
+		prev = curr->prev;
+		while (prev && prev->n > curr->n)
+		{
+			swap_nodes(list, prev);
+			print_list((const listint_t *)*list);
+			prev = curr->prev;
+		}
+		curr = curr->next;
+	}
 }
 
