6e233633253251de7f8c72300d8c5336e82270e7
allvo6377 <alvinpilot@gmail.com>
2023-04-26 01:20:47 +0000

    fix last task

--- 1000-sort_deck.c
+++ 1000-sort_deck.c
@@ -1,81 +1,93 @@
 #include "deck.h"
-
-/**
- * swap_nodes - swaps two nodes in a doubly linked list
- * @a: pointer to the first node
- * @b: pointer to the second node
- */
-void swap_nodes(deck_node_t **a, deck_node_t **b)
-{
-	deck_node_t *prev_a = (*a)->prev;
-	deck_node_t *next_b = (*b)->next;
-
-	if (prev_a)
-		prev_a->next = *b;
-	if (next_b)
-		next_b->prev = *a;
-	(*a)->next = next_b;
-	(*a)->prev = *b;
-	(*b)->next = *a;
-	(*b)->prev = prev_a;
-}
+#include <stdlib.h>
 
 /**
  * compare_cards - compares two cards by value and kind
  * @a: pointer to the first card
  * @b: pointer to the second card
- * Return: 1 if a > b, 0 if a == b, -1 if a < b
+ * Return: negative if a < b, zero if a == b, positive if a > b
  */
-int compare_cards(const card_t *a, const card_t *b)
+int compare_cards(const void *a, const void *b)
 {
-	char *values[] = {"Ace", "2", "3", "4", "5", "6", "7", "8", "9",
-			  "10", "Jack", "Queen", "King"};
-	int i, j;
+	const card_t *card_a = *(const card_t **)a;
+	const card_t *card_b = *(const card_t **)b;
+	int value_a, value_b;
 
-	for (i = 0; i < 13; i++)
-	{
-		if (strcmp(a->value, values[i]) == 0)
-			break;
-	}
-	for (j = 0; j < 13; j++)
-	{
-		if (strcmp(b->value, values[j]) == 0)
-			break;
-	}
-	if (i > j)
-		return (1);
-	if (i < j)
-		return (-1);
-	if (a->kind > b->kind)
-		return (1);
-	if (a->kind < b->kind)
-		return (-1);
-	return (0);
+	/* convert card values to integers for comparison */
+	if (card_a->value[0] == 'A')
+		value_a = 1;
+	else if (card_a->value[0] == 'J')
+		value_a = 11;
+	else if (card_a->value[0] == 'Q')
+		value_a = 12;
+	else if (card_a->value[0] == 'K')
+		value_a = 13;
+	else
+		value_a = atoi(card_a->value);
+
+	if (card_b->value[0] == 'A')
+		value_b = 1;
+	else if (card_b->value[0] == 'J')
+		value_b = 11;
+	else if (card_b->value[0] == 'Q')
+		value_b = 12;
+	else if (card_b->value[0] == 'K')
+		value_b = 13;
+	else
+		value_b = atoi(card_b->value);
+
+	/* compare cards by value and kind */
+	if (value_a != value_b)
+		return (value_a - value_b);
+	return (card_a->kind - card_b->kind);
 }
 
 /**
- * insertion_sort_list - sorts a doubly linked list of cards
- * @list: pointer to the head of the list
+ * sort_deck - sorts a deck of cards using quick sort
+ * @deck: pointer to the head of the doubly linked list
  */
-void insertion_sort_list(deck_node_t **list)
+void sort_deck(deck_node_t **deck)
 {
-	deck_node_t *curr, *insert, *temp;
+	deck_node_t *node = NULL;
+	size_t size = 0, i = 0;
+	const card_t **array = NULL;
 
-	if (!list || !*list || !(*list)->next)
+	/* check for null or single node deck */
+	if (!deck || !*deck || !(*deck)->next)
 		return;
 
-	curr = (*list)->next;
-	while (curr)
+	/* count the number of nodes in the deck */
+	node = *deck;
+	while (node)
 	{
-		insert = curr;
-		temp = curr->next;
-		while (insert->prev && compare_cards(insert->card, insert->prev->card) < 0)
-		{
-			swap_nodes(&(insert->prev), &insert);
-			if (!insert->prev)
-				*list = insert;
-			print_deck(*list);
-		}
-		curr = temp;
+		size++;
+		node = node->next;
 	}
+
+	/* create an array of pointers to the cards */
+	array = malloc(sizeof(card_t *) * size);
+	if (!array)
+		return;
+
+	node = *deck;
+	while (node)
+	{
+		array[i++] = node->card;
+		node = node->next;
+	}
+
+	/* sort the array using quick sort and compare_cards function */
+	qsort(array, size, sizeof(card_t *), compare_cards);
+
+	/* update the deck with the sorted array */
+	node = *deck;
+	i = 0;
+	while (node)
+	{
+		node->card = array[i++];
+		node = node->next;
+	}
+
+	/* free the array */
+	free(array);
 }
